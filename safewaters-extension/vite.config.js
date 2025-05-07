119a955a3daf70873ab5c1bd1128c454555a2ba7
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2025-05-07 20:29:06 -0300

    Eliminar el archivo de fondo y actualizar las rutas de los scripts en el manifiesto y la configuración de Vite

--- safewaters-extension/vite.config.js
+++ safewaters-extension/vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        popup: 'src/popup/popup.html',
-        background: 'src/background/background.js',
-        content: 'src/content/content-script.js'
+        popup: resolve(__dirname, 'src/popup/popup.html'),
+        content: resolve(__dirname, 'src/content/content.js')
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name][extname]'
       }
     }
-  }
+  },
+  publicDir: 'public' // Recursos estáticos como íconos o imágenes
 })
