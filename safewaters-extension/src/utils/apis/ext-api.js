9670f5ce82b87da7197ac36e349b90c3f52c3640
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2025-05-10 20:23:04 -0300

    Refactorizar getExtAPI para almacenar en caché la API de almacenamiento y evitar llamadas redundantes

--- safewaters-extension/src/utils/apis/ext-api.js
+++ safewaters-extension/src/utils/apis/ext-api.js
@@ -2,15 +2,29 @@
  * Obtiene la API de almacenamiento y runtime correcta para Chrome o Firefox.
  * @returns {object} - Un objeto con las APIs de almacenamiento y runtime.
  */
-export function getExtAPI() {
-    if (typeof browser !== "undefined" && browser.storage && browser.storage.local && browser.runtime) {
-        console.log("API de Firefox detectada");
-        return { storage: browser.storage.local, runtime: browser.runtime }; // Firefox
+let api = null;
+let logged = false;
 
+export function getExtAPI() {
+    if (api) {
+        return api;
+    }
+
+    if (typeof browser !== "undefined" && browser.storage && browser.storage.local && browser.runtime) {
+        if (!logged) {
+            console.log("API de Firefox detectada");
+            logged = true;
+        }
+        api = { storage: browser.storage.local, runtime: browser.runtime }; // Firefox
+        return api;
     }
     if (typeof chrome !== "undefined" && chrome.storage && chrome.storage.local && chrome.runtime) {
-        console.log("API de Chrome detectada");
-        return { storage: chrome.storage.local, runtime: chrome.runtime }; // Chrome
+        if (!logged) {
+            console.log("API de Chrome detectada");
+            logged = true;
+        }
+        api = { storage: chrome.storage.local, runtime: chrome.runtime }; // Chrome
+        return api;
     }
     throw new Error("No se encontró una API de almacenamiento compatible.");
   }
