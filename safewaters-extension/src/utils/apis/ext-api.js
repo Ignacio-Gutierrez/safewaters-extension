9f449d57f365ea35142d7a6804806cc5de81c382
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2025-05-11 18:39:21 -0300

    Refactorizar getExtAPI para estructurar el objeto de API de almacenamiento y runtime en Firefox y Chrome

--- safewaters-extension/src/utils/apis/ext-api.js
+++ safewaters-extension/src/utils/apis/ext-api.js
@@ -15,7 +15,17 @@ export function getExtAPI() {
             console.log("API de Firefox detectada");
             logged = true;
         }
-        api = { storage: browser.storage.local, runtime: browser.runtime }; // Firefox
+        api = {
+            storage: {
+                get: browser.storage.local.get.bind(browser.storage.local),
+                set: browser.storage.local.set.bind(browser.storage.local),
+                remove: browser.storage.local.remove.bind(browser.storage.local),
+                clear: browser.storage.local.clear.bind(browser.storage.local),
+                onChanged: browser.storage.onChanged
+            },
+            runtime: browser.runtime,
+            tabs: browser.tabs
+        };
         return api;
     }
     if (typeof chrome !== "undefined" && chrome.storage && chrome.storage.local && chrome.runtime) {
@@ -23,7 +33,17 @@ export function getExtAPI() {
             console.log("API de Chrome detectada");
             logged = true;
         }
-        api = { storage: chrome.storage.local, runtime: chrome.runtime }; // Chrome
+        api = {
+            storage: {
+                get: chrome.storage.local.get.bind(chrome.storage.local),
+                set: chrome.storage.local.set.bind(chrome.storage.local),
+                remove: chrome.storage.local.remove.bind(chrome.storage.local),
+                clear: chrome.storage.local.clear.bind(chrome.storage.local),
+                onChanged: chrome.storage.onChanged
+            },
+            runtime: chrome.runtime,
+            tabs: chrome.tabs
+        };
         return api;
     }
     throw new Error("No se encontró una API de almacenamiento compatible.");
