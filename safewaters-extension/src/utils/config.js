5e2967ca74b6bfae1d0e643fac077ecea427828f
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2025-05-11 22:26:29 -0300

    Eliminar la constante CACHE_EXPIRATION_MS y su comentario en config.js

--- safewaters-extension/src/utils/config.js
+++ safewaters-extension/src/utils/config.js
@@ -1,5 +1,3 @@
 export const API_BASE_URL = "http://127.0.0.1:8000";
 
 export const CACHE_KEY = "sw_url_cache";
-export const CACHE_EXPIRATION_MS = 1 * 60 * 1000; // 1 minuto
-//export const CACHE_EXPIRATION_MS = 24 * 60 * 60 * 1000; // 24 horas
