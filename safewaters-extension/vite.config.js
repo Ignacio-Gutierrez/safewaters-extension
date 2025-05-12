e2c802168778036b0e6ada4d729919e6916f0871
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2025-05-11 22:25:28 -0300

    Actualizar manifest.json y vite.config.js para mejorar la gestión de recursos y agregar el script de fondo

--- safewaters-extension/vite.config.js
+++ safewaters-extension/vite.config.js
@@ -6,8 +6,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         popup: resolve(__dirname, 'src/popup/popup.html'),
+        background: resolve(__dirname, 'src/background/background.js'),
         content: resolve(__dirname, 'src/content/content.js'),
-        'content-main': resolve(__dirname, 'src/content/content-main.js')
+        'content-main': resolve(__dirname, 'src/content/content-main.js'),
       },
       output: {
         entryFileNames: 'assets/[name].js',
