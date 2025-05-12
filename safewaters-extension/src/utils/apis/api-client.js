c4fb9a1c79957461881159a4e75f11e387140c12
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2025-05-11 22:31:03 -0300

    Actualizar la ruta de la API en la función testUrl para corregir el endpoint utilizado

--- safewaters-extension/src/utils/apis/api-client.js
+++ safewaters-extension/src/utils/apis/api-client.js
@@ -1,7 +1,7 @@
 import { API_BASE_URL } from "../config.js";
 
 export async function testUrl(url) {
-    const response = await fetch(`${API_BASE_URL}/test`, {
+    const response = await fetch(`${API_BASE_URL}/check`, {
         method: "POST",
         body: JSON.stringify({ url }),
         headers: { "Content-Type": "application/json" }
